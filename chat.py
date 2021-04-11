7a96c79692f7f9a2e7c79d53540aeea4ad05b3a2
chipala <chipala@chipala.com>
2021-04-11 15:44:37 +0200

    toto

---
+++ chat.py
@@ -0,0 +1,26 @@
+
+class Chat:
+    def __init__(self, nom, age):
+        self.nom = nom
+        self.age = age
+
+    def __str__(self):
+        return "Je m'appelle {} et j'ai {} an(s)".format(self.nom, str(self.age))
+
+    def fairePipi(self):
+        return self.__str__() + " et je fais pipi"
+
+    def faireCaca(self):
+        return self.__str__() + " et je fais caca aussi"
+
+    def faireDodo(self):
+        return self.__str__() + " et souvent je dors"
+
+
+# Main 
+chat_titi = Chat('titi', 10)
+print(chat_titi)
+print(chat_titi.fairePipi())
+
+chat_gros_minet = Chat('gros_minet', 15)
+print(chat_gros_minet.faireDodo())
